91cf60701b817697f54033247f3f7b6975bf3bed
aymather <aymather@gmail.com>
2020-09-03 09:49:10 -0700

    added remaining routes

--- models/participants.js
+++ models/participants.js
@@ -5,15 +5,27 @@ const ParticipantSchema = new Schema({
     created_at: { type: Date, default: new Date() },
     data: {
         name: { type: String, required: true },
-        subject_number: { type: String, required: true },
         age: { type: String, required: true },
         gender: { type: String, required: true },
         handedness: { type: String, required: true },
-        buttons: { type: Number, required: true }
+        buttons: { type: String, required: true },
+        training: { type: String }
     },
     trialseq_training: { type: Array, required: true, default: [] },
     trialseq: { type: Array, required: true, default: [] },
     time: { type: Array, required: true, default: [] }
 })
+
+ParticipantSchema.methods.getData = function() {
+    return {
+        id: this._id,
+        name: this.data.name,
+        age: this.data.age,
+        gender: this.data.gender,
+        handedness: this.data.handedness,
+        buttons: this.data.buttons,
+        training: this.data.training
+    }
+}
 
 module.exports = mongoose.model('participants', ParticipantSchema);
--- routes/participants.js
+++ routes/participants.js
@@ -3,23 +3,52 @@ const { Participant } = require('../models');
 
 const router = express.Router();
 
-router.get('/p/save', async (req, res) => {
+router.get('/p/all', async (req, res) => {
 
-    const p = new Participant({
-        data: {
-            name: 'Alec Mather',
-            subject_number: '001',
-            age: '23',
-            gender: 'M',
-            handedness: 'R',
-            buttons: 1
+    try {
+
+        const participants = await Participant.find();
+        const ps = participants.map(p => p.getData());
+        return res.json(ps);
+
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+
+})
+
+router.post('/p/new', async (req, res) => {
+
+    try {
+        const p = new Participant({
+            data: req.body
+        })
+        const sp = await p.save();
+        return res.json(sp.getData());
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+
+})
+
+router.post('/p/save', async (req, res) => {
+
+    try {
+        const { participant, sequence } = req.body;
+    
+        const p = await Participant.findById(participant.id);
+        if(participant.training === "1") {
+            p.trialseq_training = sequence
+        } else {
+            p.trialseq = sequence
         }
-    })
 
-    console.log(p);
-    const sp = await p.save();
+        await p.save()
 
-    return res.json(sp);
+        return res.json(req.body);
+    } catch (error) {
+        return res.status(500).json(error)
+    }
 
 })
 
